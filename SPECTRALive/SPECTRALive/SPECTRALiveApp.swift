7afc7c480b0406f66ef778a1e774d0156745072b
ajayshahdesign-beep <ajayshahdesign@gmail.com>
2026-04-25 01:23:28 -0700

    First build of SPECTRALive app

---
+++ SPECTRALive/SPECTRALive/SPECTRALiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPECTRALiveApp.swift
+//  SPECTRALive
+//
+//  Created by Ajay Shah on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct SPECTRALiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
